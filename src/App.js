41b6f31ff61cc7812340f1bb3ca1f8b8e082a90e
natacha <nat_bartu@hotmail.com>
2022-03-14 12:59:39 +1000

    rendered list items and display on dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
 import Accordion from "./components/accordion/Accordion";
 
-function App() {
+const items = [
+  {
+    title: "What is React?",
+    content: "React is front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
+
+const App = () => {
   return (
     <div>
-      <Accordion />
+      <Accordion items={items} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/accordion/Accordion.js
+++ src/components/accordion/Accordion.js
@@ -1,11 +1,23 @@
 import React from "react";
 
-function Accordion() {
+const Accordion = ({ items }) => {
+  const renderedItems = items.map((item) => {
+    return (
+      <React.Fragment key={item.title}>
+        <div className="title active">
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className="content active"></div>
+        <p>{item.content}</p>
+      </React.Fragment>
+    );
+  });
   return (
-    <div className="container">
-      <div>Accordion</div>
+    <div className="ui styled accordion">
+      <h1>{renderedItems}</h1>
     </div>
   );
-}
+};
 
 export default Accordion;
