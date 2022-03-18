03295bb89d83ba8ebb5861cb686193aa36688440
natacha <nat_bartu@hotmail.com>
2022-03-18 14:47:05 +1000

    set convert output when user type on input

--- src/components/convert/Convert.js
+++ src/components/convert/Convert.js
@@ -4,23 +4,30 @@ import axios from "axios";
 const KEY = process.env.REACT_APP_WIDGETS_KEY;
 
 const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState("");
+
   useEffect(() => {
-    axios.post(
-      "https://translation.googleapis.com/language/translate/v2",
-      {},
-      {
-        params: {
-          q: text,
-          target: language.value,
-          key: KEY,
-        },
-      }
-    );
+    const doTranslation = async () => {
+      const { data } = await axios.post(
+        "https://translation.googleapis.com/language/translate/v2",
+        {},
+        {
+          params: {
+            q: text,
+            target: language.value,
+            key: KEY,
+          },
+        }
+      );
+      setTranslated(data.data.translations[0].translatedText);
+    };
+
+    doTranslation();
   }, [language, text]);
   return (
-    <>
-      <p>convert</p>
-    </>
+    <div>
+      <h1 className="ui header">{translated}</h1>
+    </div>
   );
 };
 
