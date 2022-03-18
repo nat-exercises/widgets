58000b3a6e6aa5fedaf3f26f0687109df36999ce
natacha <nat_bartu@hotmail.com>
2022-03-18 14:38:23 +1000

    add api key on convert compoennet

--- src/components/convert/Convert.js
+++ src/components/convert/Convert.js
@@ -1,8 +1,21 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const KEY = process.env.REACT_APP_WIDGETS_KEY;
 
 const Convert = ({ language, text }) => {
   useEffect(() => {
-    console.log("new language or text");
+    axios.post(
+      "https://translation.googleapis.com/language/translate/v2",
+      {},
+      {
+        params: {
+          q: text,
+          target: language.value,
+          key: KEY,
+        },
+      }
+    );
   }, [language, text]);
   return (
     <>
